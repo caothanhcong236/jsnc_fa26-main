26837913a3bdf73f6c200c3b7b5548c2fb89a8b0
Thanh Cong <caothanhcong236@gmail.com>
2026-09-15 15:56:32 +0700

    lab1

---
+++ lab1.js
@@ -0,0 +1,9 @@
+let name = "Cao Thành Công";
+let age = 19;
+let address = "Hà Nội";
+let isStudent = true;
+
+console.log("Họ tên:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
