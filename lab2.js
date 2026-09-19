bdf7d987cd04fbebe9651733ad4e9e3b7464e692
Thanh Cong <caothanhcong236@gmail.com>
2026-09-19 15:45:35 +0700

    update lab2

--- lab2.js
+++ lab2.js
@@ -17,3 +17,36 @@
     console.log(names[i]);
   }
 }
+
+// Bài 3
+const student = {
+  id: 1,
+  name: "Nguyễn Văn An",
+  age: 20,
+  email: "an@gmail.com",
+  major: "CNTT"
+};
+
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.email);
+
+student.age = 21;
+student.phone = "0123456789";
+console.log(student);
+
+// Bài 4
+const students = [
+  { id: 1, name: "Nguyễn Văn An", age: 20 },
+  { id: 2, name: "Trần Văn Bình", age: 21 },
+  { id: 3, name: "Lê Văn Nam", age: 20 }
+];
+
+console.log(students);
+console.log(students[0].name);
+console.log(students[1].age);
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i].name);
+}
